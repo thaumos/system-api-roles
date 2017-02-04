a17710626eec8e8526d115c3878f7f03321a04f2
Lars Karlitski <lars@karlitski.net>
2017-02-04 23:18:01 +0100

    test: add tuned test

---
+++ test/test-tuned.py
@@ -0,0 +1,24 @@
+
+import machine
+import re
+
+
+class TestTuned(machine.Test):
+
+    """
+    :avocado: enable
+    """
+
+    def test(self):
+        for profile in ('balanced', 'powersave', 'throughput-performance', 'latency-performance'):
+            self.machine.set_config('com.redhat.tuned', profile=profile)
+            self.assertEqual(profile, self.get_active_profile())
+
+        recommended = self.machine.execute('sudo tuned-adm recommend').stdout.strip()
+        self.machine.set_config('com.redhat.tuned', use_recommended_profile=True)
+        self.assertEqual(recommended, self.get_active_profile())
+
+    def get_active_profile(self):
+        output = self.machine.execute('sudo tuned-adm active').stdout
+        return re.match('^.*: (.*)$', output).group(1)
+
